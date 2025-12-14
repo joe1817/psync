bc3de0a17658e13d9cac6358581084a356cc53cf
Joe Walter <1817@duck.com>
2025-12-13 20:56:49 -0500

    updated help text

--- psync/__main__.py
+++ psync/__main__.py
@@ -15,57 +15,56 @@ class _ArgParser:
 	'''Argument parser for when this python file is run with arguments instead of an imported package.'''
 
 	parser = argparse.ArgumentParser(
-		description="Copy new and updated files from one directory to another.",
-		epilog="(c) 2025 Joe Walter",
-		fromfile_prefix_chars="!",
+		description = "Copy files from one directory to another.",
+		fromfile_prefix_chars = "!",
 	)
 
 	parser.add_argument("src", help="The root directory to copy files from.")
 	parser.add_argument("dst", help="The root directory to copy files to.")
 
-	parser.add_argument("-f", "--filter", metavar="filter_string", nargs="+", type=str, default=None, help="The filter string that includes/excludes file system entries from the 'src' and 'dst' directories. Similar to rsync, the format of the filter string is one of more repetitions of: (+ or -), followed by a list of one of more relative path patterns. Including (+) or excluding (-) of file system entries is determined by the preceding symbol of the first matching pattern. Included files will be copied over as part of the backup, while included directories will be searched. Each pattern ending with \"/\" will apply to directories only. Otherise the pattern will apply only to files. (Defaults to \"+ **/*\", which searches all directories and copies all files.)")
-	parser.add_argument("-ih", "--ignore-hidden", action="store_true", default=None, help="Ignore hidden files by default in glob patterns. That is, wildcards in glob patterns will not match file system entries beginning with a dot. However, globs containing a dot (e.g., \"**/.*\") will still match these file system entries.")
-	parser.add_argument("-ic", "--ignore-case", action="store_true", default=None, help="Ignore case when comparing files to the filter string.")
+	sync_options = parser.add_argument_group("Syncing")
+	sync_options.add_argument("-cf", "--create-files", action=BOA, default=None, help="Enable/disable file creation in 'dst'.")
+	sync_options.add_argument("-cd", "--create-dir-tree", action=BOA, default=None, help="Enable/disable creation of the directory tree from 'src' in 'dst'.")
+	sync_options.add_argument("--renames", action=BOA, default=None, help="Enable/disable file or directory renaming in 'dst'.")
+	sync_options.add_argument("-xf", "--delete-files", action=BOA, default=None, help="Enable/disable file deletion in 'dst'. If 'trash' is set, then files will be moved into it instead of deleted.")
+	sync_options.add_argument("-xd", "--delete-empty-dirs", action=BOA, default=None, help="Enable/disable deletion of empty directories in 'dst'. If 'trash' is set, then empty directories will be moved into it instead of deleted.")
+	sync_options.add_argument("-t", "--trash", metavar="PATH", nargs="?", type=str, const="auto", default=None, help="The root directory to move 'extra' files (those that are in 'dst' but not 'src'). Must be on the same file system as 'dst'. If set to \"auto\", then a directory will automatically be made next to 'dst'. Extra files will not be moved if this option is omitted.")
+	sync_options.add_argument("-fu", "--force-update", action=BOA, default=None, help="Enable/disable replacement of any files in 'dst' with older copies in 'src'.")
+	sync_options.add_argument("-fr", "--force-replace", action=BOA, default=None, help="Enable/disable replacement of files with directories (or vice versa) when their names match.")
+	sync_options.add_argument("-g", "--global-renames", action=BOA, default=None, help="Enable/disable renaming files between directories.")
+	sync_options.add_argument("-c", "--content-match", action=BOA, default=None, help="Enable/disable reading the last 1kb of files when finding renamed files in 'dst'. If false, the backup process will rely solely on file metadata.")
+	sync_options.add_argument("--rename-threshold", metavar="SIZE", type=int, default=None, help="Set the minimum size in bytes needed to consider renaming files in dst to match those in 'src'. Renamed files below this threshold will be simply deleted in dst and their replacements copied over.")
+	sync_options.add_argument("-m", "--mirror", action="store_true", default=None, help="Equivalent to --create-dir-tree, --delete-files, --force-update, and --force-replace.")
+	sync_options.add_argument("-d", "--dry-run", action="store_true", default=None, help="Forgo performing any operation that would make a file system change. Changes that would have occurred will still be logged.")
+	sync_options.add_argument("-L", "--err-limit", metavar="LIMIT", type=int, default=None, help="Quit after this many filesystem errors.")
 
-	parser.add_argument("--symlink-translation", action=BOA, default=None, help="Enable/disable symbolic link path transaltion on 'dst'. If false, symlinks will be copied exactly.")
-	symlink_handling = parser.add_mutually_exclusive_group()
-	symlink_handling.add_argument("--ignore-symlinks", action="store_true", default=None, help="Ignore symbolic links under 'src' and 'dst'. Note that 'src' and 'dst' themselves will be followed regardless of this flag.")
-	symlink_handling.add_argument("--follow-symlinks", action="store_true", default=None, help="Follow symbolic links under 'src' and 'dst'. Note that 'src' and 'dst' themselves will be followed regardless of this flag.")
+	filter_options = parser.add_argument_group("Filtering")
+	filter_options.add_argument("-f", "--filter", metavar="STR", nargs="+", type=str, default=None, help="Set the filter string that includes/excludes file system entries from the 'src' and 'dst' directories. Similar to rsync, the format of the filter string is one of more repetitions of: (+ or -), followed by a list of one of more relative path patterns. Including (+) or excluding (-) of file system entries is determined by the preceding symbol of the first matching pattern. Included files will be copied over as part of the backup, while included directories will be searched. Each pattern ending with \"/\" will apply to directories only. Otherise the pattern will apply only to files. (Defaults to \"+ **/*\", which searches all directories and copies all files.)")
+	filter_options.add_argument("-ih", "--ignore-hidden", action="store_true", default=None, help="Ignore hidden files by default in glob patterns. That is, wildcards in glob patterns will not match file system entries beginning with a dot. However, globs containing a dot (e.g., \"**/.*\") will still match these file system entries.")
+	filter_options.add_argument("-ic", "--ignore-case", action="store_true", default=None, help="Ignore case when comparing files to the filter string.")
 
-	parser.add_argument("-cf", "--create-files", action=BOA, default=None, help="Enable/disable file creation in 'dst'.")
-	parser.add_argument("-cd", "--create-dir-tree", action=BOA, default=None, help="Enable/disable creation of the directory tree from 'src' in 'dst'.")
-	parser.add_argument("--renames", action=BOA, default=None, help="Enable/disable file or directory renaming in 'dst'.")
-	parser.add_argument("-xf", "--delete-files", action=BOA, default=None, help="Enable/disable file deletion in 'dst'. If 'trash' is set, then files will be moved into it instead of deleted.")
-	parser.add_argument("-xd", "--delete-empty-dirs", action=BOA, default=None, help="Enable/disable deletion of empty directories in 'dst'. If 'trash' is set, then empty directories will be moved into it instead of deleted.")
-	parser.add_argument("-t", "--trash", metavar="path", nargs="?", type=str, const="auto", default=None, help="The root directory to move 'extra' files (those that are in 'dst' but not 'src'). Must be on the same file system as 'dst'. If set to \"auto\", then a directory will automatically be made next to 'dst'. Extra files will not be moved if this option is omitted.")
+	symlink_handling = parser.add_argument_group("Symlink Handling")
+	symlink_handling.add_argument("--symlink-translation", action=BOA, default=None, help="Enable/disable symbolic link path transaltion on 'dst'. If false, symlinks will be copied exactly.")
+	symlink_following = symlink_handling.add_mutually_exclusive_group()
+	symlink_following.add_argument("--ignore-symlinks", action="store_true", default=None, help="Ignore symbolic links under 'src' and 'dst'. Note that 'src' and 'dst' themselves will be followed regardless of this flag.")
+	symlink_following.add_argument("--follow-symlinks", action="store_true", default=None, help="Follow symbolic links under 'src' and 'dst'. Note that 'src' and 'dst' themselves will be followed regardless of this flag.")
 
-	parser.add_argument("-fu", "--force-update", action=BOA, default=None, help="Enable/disable replacement of any files in 'dst' with older copies in 'src'.")
-	parser.add_argument("-fr", "--force-replace", action=BOA, default=None, help="Enable/disable replacement of files with directories (or vice versa) when their names match.")
-	parser.add_argument("-g", "--global-renames", action=BOA, default=None, help="Enable/disable renaming files between directories.")
-	parser.add_argument("-c", "--content-match", action=BOA, default=None, help="Enable/disable reading the last 1kb of files when finding renamed files in 'dst'. If false, the backup process will rely solely on file metadata.")
-	parser.add_argument("--rename-threshold", metavar="size", type=int, default=None, help="The minimum size in bytes needed to consider renaming files in dst to match those in 'src'. Renamed files below this threshold will be simply deleted in dst and their replacements copied over.")
-	parser.add_argument("-m", "--mirror", action="store_true", default=None, help="Equivalent to --create-dir-tree, --delete-files, --force-update, and --force-replace.")
-
-	parser.add_argument("--shutdown-src", action="store_true", default=None, help="Shutdown the src system when done.")
-	parser.add_argument("--shutdown-dst", action="store_true", default=None, help="Shutdown the dst system when done.")
-	parser.add_argument("-L", "--err-limit", metavar="limit", type=int, default=None, help="Quit after this many filesystem errors.")
-	parser.add_argument("-d", "--dry-run", action="store_true", default=None, help="Forgo performing any operation that would make a file system change. Changes that would have occurred will still be printed to console.")
-
-	parser.add_argument("-w", "--watch", action="store_true", default=None, help="Will watch the 'src' directory and automatically sync filesystem changes.")
-
-	parser.add_argument("--log", metavar="path", nargs="?", type=str, const="auto", default=None, help="The path of the log file to use. It will be created if it does not exist. With \"auto\" or no argument, a tempfile will be used for the log, and it will be moved to the user's home directory after the backup is done. If this flag is absent, then no logging will be performed.")
-	parser.add_argument("--log-level", type=str, default=None, help="Log level for logging to file.")
-
-	print_level = parser.add_mutually_exclusive_group()
+	log_options = parser.add_argument_group("Logging")
+	log_options.add_argument("--log", metavar="PATH", nargs="?", type=str, const="auto", default=None, help="Set the log file. It will be created if it does not exist. With \"auto\" or no argument, a tempfile will be used for the log, and it will be moved to the user's home directory after the backup is done. If this flag is absent, then no logging will be performed.")
+	log_options.add_argument("--log-level", metavar="LEVEL", type=str, default=None, help="Set the log level for the file logger.")
+	print_level = log_options.add_mutually_exclusive_group()
+	print_level.add_argument("-p", "--print-level", metavar="LEVEL", type=str, default=None, help="Set the log level for the console logger.")
 	print_level.add_argument("-q", action="count", default=None, help="Shorthand for --print-level WARNING (-q) and --print-level CRITICAL (-qq).")
-	print_level.add_argument("-p", "--print-level", type=str, default=None, help="Log level for printing to console.")
+	log_options.add_argument("--debug", action="store_true", default=None, help="Shorthand for --print-level DEBUG and --log-level DEBUG.")
+	log_options.add_argument("--title", metavar="STR", nargs="+", type=str, default=None, help="Set the title of the sync operation, which will be printed in the header.")
+	log_options.add_argument("-nh", "--no-header", action="store_true", default=None, help="Skip logging header information.")
+	log_options.add_argument("-nf", "--no-footer", action="store_true", default=None, help="Skip logging footer information.")
+	log_options.add_argument("-nhf", "--no-header-or-footer", action="store_true", default=None, help="Skip logging header and footer information.")
 
-	parser.add_argument("--debug", action="store_true", default=None, help="Shorthand for --print-level DEBUG and --log-level DEBUG.")
-
-	parser.add_argument("--title", metavar="title", nargs="+", type=str, default=None, help="A strng to be printed in the header.")
-	parser.add_argument("-nh", "--no-header", action="store_true", default=None, help="Skip logging header information.")
-	parser.add_argument("-nf", "--no-footer", action="store_true", default=None, help="Skip logging footer information.")
-	parser.add_argument("-nhf", "--no-header-or-footer", action="store_true", default=None, help="Skip logging header and footer information.")
+	misc_options = parser.add_argument_group("Miscellaneous")
+	misc_options.add_argument("--shutdown-src", action="store_true", default=None, help="Shutdown the src system when done.")
+	misc_options.add_argument("--shutdown-dst", action="store_true", default=None, help="Shutdown the dst system when done.")
+	misc_options.add_argument("-w", "--watch", action="store_true", default=None, help="Watch 'src' and automatically sync filesystem changes to 'dst'.")
 
 	@staticmethod
 	def parse(args:list[str]) -> argparse.Namespace:
